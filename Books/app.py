77acae294ca6d9e9c00a9b1ea6d3ed9222788d5e
jiri.palek <jiri.palek@blindspot.ai>
2018-11-13 10:06:05 +0100

    Returning a book

--- Books/app.py
+++ Books/app.py
@@ -15,13 +15,25 @@ books = [
     }
 ]
 
+# GET /books/<isbn>
+@app.route("/books/<int:isbn>")
+def get_book_by_isbn(isbn):
+    return_value = {}
+    for book in books:
+        if book["isbn"] == isbn:
+            return_value = {
+                "name": book["name"],
+                "price": book["price"]
+            }
+    return jsonify(return_value)
+
 # GET /books
 @app.route("/books")
 def get_books():
     return jsonify({"books": books})
 
 @app.route("/")
-def hello_world():
-    return "Hello world"
+def initial_page():
+    return "<b><font size=\"+2\"> This is an intial page of Books Application </font></b>"
 
 app.run(port=5000)
