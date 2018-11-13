fd9df936f8de2a991d7afb311cb13527e5331297
jiri.palek <jiri.palek@blindspot.ai>
2018-11-13 09:51:02 +0100

    Returning books

--- Books/app.py
+++ Books/app.py
@@ -1,7 +1,24 @@
 from flask import Flask
+from flask import jsonify
 
 app = Flask(__name__)
-print(__name__)
+books = [
+    {
+        "name": "Green Eggs and Ham",
+        "price": 7.99,
+        "isbn": 123456
+    },
+    {
+        "name": "The Cat in the Hat",
+        "price": 6.99,
+        "isbn": 234567
+    }
+]
+
+# GET /books
+@app.route("/books")
+def get_books():
+    return jsonify({"books": books})
 
 @app.route("/")
 def hello_world():
